c7147743634b3d6542fbc51b650c24cdc61d815a
Jonathan Beilin <jbeilin@gmail.com>
2011-03-30 15:17:31 -0700

    lgpl stuff added

--- src/mSoundManager.mm
+++ src/mSoundManager.mm
@@ -1,10 +1,23 @@
 /*
  *  mSoundManager.cpp
- *  mandala
+ *  Yantra Mandala
  *
- *  Created by Jonathan Beilin on 10/31/10.
- *  Copyright 2010 Koduco Games. All rights reserved.
+ *  Created by Jonathan Beilin on 10/28/10.
+ *  Copyright 2010 Jonathan Beilin. All rights reserved.
  *
+ This file is part of Yantra Mandala.
+ 
+ Yantra Mandala is free software: you can redistribute it and/or modify
+ it under the terms of the Lesser GNU General Public License as published by
+ the Free Software Foundation.
+ 
+ Yantra Mandala is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Yantra Mandala.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "mSoundManager.h"
